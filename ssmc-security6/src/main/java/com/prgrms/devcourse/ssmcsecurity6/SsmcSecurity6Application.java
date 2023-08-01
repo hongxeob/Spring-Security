938a8981fcfa76ce1a2a981951263bc224156c57
Hongseob Lee <sjun2918@naver.com>
2023-08-01 16:04:28 +0900

    security6 학습용 모듈 생성

---
+++ ssmc-security6/src/main/java/com/prgrms/devcourse/ssmcsecurity6/SsmcSecurity6Application.java
@@ -0,0 +1,13 @@
+package com.prgrms.devcourse.ssmcsecurity6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmcSecurity6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmcSecurity6Application.class, args);
+    }
+
+}
---
+++ ssmc-security6/src/test/java/com/prgrms/devcourse/ssmcsecurity6/SsmcSecurity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.devcourse.ssmcsecurity6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmcSecurity6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
