b78eca835371f9b3359b0d82ce01d267dca4829d
Hongseob Lee <sjun2918@naver.com>
2023-08-02 13:40:56 +0900

    security6 - week1/day2 - 미션

--- ssmc-security6/src/main/java/com/prgrms/devcourse/ssmcsecurity6/config/WebSecurityConfigure.java
+++ ssmc-security6/src/main/java/com/prgrms/devcourse/ssmcsecurity6/config/WebSecurityConfigure.java
@@ -1,19 +1,28 @@
 package com.prgrms.devcourse.ssmcsecurity6.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
@@ -45,35 +54,50 @@ public class WebSecurityConfigure {
     @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .authorizeRequests()
+                .authorizeRequests()
                 .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/admin").access("isFullyAuthenticated() and hasRole('ADMIN')")
                 .anyRequest().permitAll()
                 .and()
-            .formLogin()
+                .formLogin()
                 .defaultSuccessUrl("/")
                 .permitAll()
                 .and()
-            /**
-             * remember me 설정
-             */
-            .rememberMe()
+                /**
+                 * remember me 설정
+                 */
+                .rememberMe()
                 .rememberMeParameter("remember-me")
                 .tokenValiditySeconds(300)
                 .and()
-            /**
-             * 로그아웃
-             */
-            .logout()
+                /**
+                 * 로그아웃
+                 */
+                .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .and()
-            /**
-             * HTTP 요청을 HTTPS 요청으로 리다이렉트
-             */
-            .requiresChannel()
+                /**
+                 * HTTP 요청을 HTTPS 요청으로 리다이렉트
+                 */
+                .requiresChannel()
                 .anyRequest().requiresSecure()
                 .and().build();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, e) -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principal = authentication != null ? authentication.getPrincipal() : null;
+            log.warn("{} is denied", principal, e);
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("text/plain;charset=UTF-8");
+            response.getWriter().write("ACCESS DENIED");
+            response.getWriter().flush();
+            response.getWriter().close();
+        };
     }
 }
