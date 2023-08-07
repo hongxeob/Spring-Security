78398ab6c8b4a0862ba2b0af9f7c2da5889b88b3
Hongseob Lee <sjun2918@naver.com>
2023-08-07 13:12:12 +0900

    security6 - week2/day2

--- ssmc-security6/src/main/java/com/prgrms/devcourse/ssmcsecurity6/user/UserRepository.java
+++ ssmc-security6/src/main/java/com/prgrms/devcourse/ssmcsecurity6/user/UserRepository.java
@@ -2,11 +2,12 @@ package com.prgrms.devcourse.ssmcsecurity6.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-//    @Query("select u from User u join fetch u.group g left join fetch g.permissions gp join fetch gp.permission where u.loginId = :loginId")
-    Optional<User> findByLoginId(String loginId);
+    @Query("select u from User u join fetch u.group g left join fetch g.permissions gp join fetch gp.permission where u.loginId = :loginId")
+    Optional<User> findByLoginId(@Param("loginId") String loginId);
 }
