07fc2a48095ccd5b5d4d4027545fe978776ba2aa
Hongseob Lee <sjun2918@naver.com>
2023-08-07 14:43:33 +0900

    security6 - week2/day3

---
+++ ssmc-security6/src/main/java/com/prgrms/devcourse/ssmcsecurity6/config/JwtConfig.java
@@ -0,0 +1,19 @@
+package com.prgrms.devcourse.ssmcsecurity6.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "jwt")
+@Component
+@Data
+public class JwtConfig {
+
+    private String header;
+
+    private String issuer;
+
+    private String clientSecret;
+
+    private int expirySeconds;
+}
